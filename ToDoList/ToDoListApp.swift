383b3a71a6c88a4ac05a6721dd67cc56b030e36c
burhanj888 <burhanj888@gmail.com>
2023-10-25 11:10:19 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Burhanuddin Jinwala on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
